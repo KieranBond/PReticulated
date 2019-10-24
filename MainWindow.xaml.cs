ae4171364f0404cf392dd0fca7aadb73ea899bd9
Kieran Bond <kieran.bond@dbpixelhouse.com>
2019-10-24 09:43:55 +0100

    Removed unneeded usings

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,11 +3,9 @@ using Microsoft.WindowsAPICodePack.Dialogs;
 using PdfiumViewer;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace PDFConverter
 {
